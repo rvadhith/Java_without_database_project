8e07559c8af863b23dd771fd76798a322509bf54
rvadhith <61987696+rvadhith@users.noreply.github.com>
2020-05-14 11:56:49 +0530

    Second commit

--- Pizza_Restaurant_Management_System/src/order/IngredientsMap.java
+++ Pizza_Restaurant_Management_System/src/order/IngredientsMap.java
@@ -2,41 +2,71 @@ package order;
 
 
 import java.util.LinkedHashMap;
+import java.util.Map.Entry;
 
 public class IngredientsMap {
-	public static LinkedHashMap<String, Integer> getCrustMap() {
-		LinkedHashMap<String, Integer> crust = new LinkedHashMap<String, Integer>();
-		crust.put("Wheat thin crust", 100);
-		crust.put("Thick crust", 110);
-		crust.put("Classic hand tossed", 120);
-		
-		return crust;
-	}
 	
-	public static LinkedHashMap<String, Integer> getCheeseMap() {
-		LinkedHashMap<String, Integer> cheese = new LinkedHashMap<String, Integer>();
-		cheese.put("Single cheese", 15);
-		cheese.put("Extra cheese", 30);
-		cheese.put("No cheese", 0);
-		
-		return cheese;
-	}
+	public static LinkedHashMap<String, Integer> crustMap = new LinkedHashMap<String, Integer>() {{ 
+        put("Wheat thin crust", 100); 
+        put("Thick crust", 110); 
+        put("Classic Hand Tossed", 120); 
+    }};
+    
+    public static LinkedHashMap<String, Integer> getCrustMap() {
+    	return crustMap;
+    }
+    
+    public static void printCrustMap() {
+    	for( Entry<String, Integer> entry : getCrustMap().entrySet() ){
+		    System.out.println( entry.getKey() + " => " + entry.getValue() );
+		}
+    }
+    
+    public static LinkedHashMap<String, Integer> cheeseMap = new LinkedHashMap<String, Integer>() {{ 
+        put("Single cheese", 15); 
+        put("Extra cheese", 30); 
+        put("No cheese", 0); 
+    }};
+    
+    public static LinkedHashMap<String, Integer> getCheeseMap() {
+    	return cheeseMap;
+    }
 	
-	public static LinkedHashMap<String, Integer> getVegBaseToppingMap() {
-		LinkedHashMap<String, Integer> vegBaseTopping = new LinkedHashMap<String, Integer>();
-		vegBaseTopping.put("Paneer", 50);
-		vegBaseTopping.put("Capsicum", 30);
-		vegBaseTopping.put("Corn", 40);
-		
-		return vegBaseTopping;
-	}
-	
-	public static LinkedHashMap<String, Integer> getNonVegBaseToppingMap() {
-		LinkedHashMap<String, Integer> nonvegBaseTopping = new LinkedHashMap<String, Integer>();
-		nonvegBaseTopping.put("Chicken Sausage", 70);
-		nonvegBaseTopping.put("BBQ Chicken", 80);
-		nonvegBaseTopping.put("Chicken Salami", 90);
-		
-		return nonvegBaseTopping;
-	}
+    public static void printCheeseMap() {
+    	for( Entry<String, Integer> entry : getCheeseMap().entrySet() ){
+		    System.out.println( entry.getKey() + " => " + entry.getValue() );
+		}
+    }
+    
+    public static LinkedHashMap<String, Integer> vegBaseToppingMap = new LinkedHashMap<String, Integer>() {{ 
+        put("Paneer", 50); 
+        put("Capsicum", 30); 
+        put("Corn", 40); 
+    }};
+    
+    public static LinkedHashMap<String, Integer> getVegBaseToppingMap() {
+    	return vegBaseToppingMap;
+    }
+    
+    public static void printVegBaseToppingMap() {
+    	for( Entry<String, Integer> entry : getVegBaseToppingMap().entrySet() ){
+		    System.out.println( entry.getKey() + " => " + entry.getValue() );
+		}
+    }
+    
+    public static LinkedHashMap<String, Integer> nonVegBaseToppingMap = new LinkedHashMap<String, Integer>() {{ 
+        put("Chicken Sausage", 70); 
+        put("BBQ Chicken", 80); 
+        put("Chicken Salami", 90); 
+    }};
+    
+    public static LinkedHashMap<String, Integer> getNonVegBaseToppingMap() {
+    	return nonVegBaseToppingMap;
+    }
+    
+    public static void printNonVegBaseToppingMap() {
+    	for( Entry<String, Integer> entry : getNonVegBaseToppingMap().entrySet() ){
+		    System.out.println( entry.getKey() + " => " + entry.getValue() );
+		}
+    }
 }
